56f9bb89fb69e2a3ac8e3c19f3d4eebe5780b0bf
janaite <jorge@janaite.net>
2024-10-26 19:58:41 -0300

    display Hello (calling efi by hand)

--- readme.md
+++ readme.md
@@ -1,4 +1,3 @@
-
 # Simple barebones Rust OS
 
 This example will generate a simple OS kernl bootable by UEFI system
@@ -12,6 +11,16 @@ cargo +nightly build
 cargo build
 ```
 
+## testing
+
+- create a directory named `/EFI/boot` and rename boot file as `Bootx64.efi`
+- execute Qemmu at root of this directory. Example: `/mnt/w/work/rust-os/rustos-boot-uefi/target/x86_64-unknown-uefi/debug/EFI/boot/Bootx64.efi`
+
+```
+qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -drive format=raw,file=fat:rw:/mnt/w/work/rust-os/rustos-boot-uefi/target/x86_64-unknown-uefi/debug/
+```
+
 ## Reference:
-* <https://medium.com/@applepies12/writing-an-os-in-rust-part-1-bb310ff2ee6d>
-* <https://medium.com/@applepies12/writing-an-os-in-rust-part-2-8a424f71e6ae>
+
+- <https://medium.com/@applepies12/writing-an-os-in-rust-part-1-bb310ff2ee6d>
+- <https://medium.com/@applepies12/writing-an-os-in-rust-part-2-8a424f71e6ae>
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,30 @@
 
 use core::panic::PanicInfo;
 
+mod uefi;
+use uefi::*;
 #[no_mangle]
-pub extern "efiapi" fn efi_main() { 
+pub extern "efiapi" fn efi_main(_handle: ImageHandle, system_table: *const SystemTable) {
+    let msg = "Hello\n\r";
+    for character in msg.chars() {
+        let mut buffer: [u16; 1] = [0];
+        let utf16 = character.encode_utf16(&mut buffer);
+        unsafe {
+            let _status =
+                ((*(*system_table).output).output_string)((*system_table).output, &utf16[0]);
+        }
+    }
+
+    //let string_arr = ['h' as u16, 'i' as u16, '!' as u16, '\n' as u16, '\0' as u16];
+    let string_arr = [
+        'W' as u16, 'o' as u16, 'r' as u16, 'l' as u16, 'd' as u16, '!' as u16,
+    ];
+
+    unsafe {
+        let _status =
+            ((*(*system_table).output).output_string)((*system_table).output, &string_arr[0]);
+    }
+    loop {}
 }
 
 #[panic_handler]
